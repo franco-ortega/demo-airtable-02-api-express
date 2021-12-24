91bb988d7a72f934010d7af436284d7f1f7f7197
Franco Ortega <francopdx@gmail.com>
2021-12-23 16:32:25 -0800

    add app; refactor server

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
+const app = require('./app');
 const PORT = 3500;
-
-app.use(cors());
-
-app.get('/', (req, res, next) => {
-    res.send('Welcome to the Maps server.');
-});
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}.`)
